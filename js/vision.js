d087319e31ef2fa12ce197b73a8e80dd99c8fe88
your <you@example.com>
2017-11-01 12:54:39 -0500

    modified:   js/vision.js

--- js/vision.js
+++ js/vision.js
@@ -1,4 +1,4 @@
-/*$(document).ready(function(){
+$(document).ready(function(){
 	$("#sendImg").click(function() {
 		var API_KEY = "AIzaSyCOozKtMmJROwMSK1hEThdiUG4XUIOs8YU";
 		var data = new FormData();
@@ -9,17 +9,19 @@
 		  type: 'POST',
           url: 'https://vision.googleapis.com/v1alpha1/images:annotate?key=' + API_KEY,
           data: data,
+          processData: false;
+          contentType: false;
           success: function(data) {
-          	$("#results").html("hola mundo");       
+          	$("#results").html(data);       
           }		 	
 		 })
 	}
 	);
-});*/
+});
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$("#sendImg").click(function() {
 		$("#results").html("<b>Hola mundo</b>");
 		alert("ok");
 	});
-});
+});*/
