03a5b45305889224a43c272d0dec72976123e920
your <you@example.com>
2017-11-09 00:20:38 -0500

    modified:   html/index.html 	modified:   js/vision.js

--- js/vision.js
+++ js/vision.js
@@ -1,16 +1,32 @@
+
 $(document).ready(function(){
+	$("#upImg").change(function previewFile(){
+		var preview = document.querySelector("img");
+		var file    = document.querySelector('input[type=file]').files[0];
+		var reader = new FileReader();
+		reader.onloadend = function(){
+			preview.src = reader.result;
+		}
+		if (file) {
+			reader.readAsDataURL(file);
+		} else {
+			preview.src = "";
+		}
+	});
+
 	$("#sendImg").click(function() {
 		var API_KEY = "AIzaSyCOozKtMmJROwMSK1hEThdiUG4XUIOs8YU";
-		var data = new FormData();
+		var data = document.querySelector("img").src;
+		/*var data = new FormData();
 		jQuery.each($("#upImg")[0].files, function(i, file) {
     		data.append('file-'+i, file);
-		});
+		});*/
 		 $.ajax({
 		  type: 'POST',
           url: 'https://vision.googleapis.com/v1alpha1/images:annotate?key=' + API_KEY,
           data: data,
-          processData: false;
-          contentType: false;
+          processData: false,
+          contentType: false,
           success: function(data) {
           	$("#results").html(data);       
           }		 	
